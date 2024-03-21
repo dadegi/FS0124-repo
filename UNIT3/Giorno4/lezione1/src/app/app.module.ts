b6b5bd183464fe17963a1e346bef30b3f1329ec7
Naviganet <dadegi@gmail.com>
2024-03-21 11:33:04 +0100

    Codice lezione UNIT3 settimana 1 giorno 4

--- UNIT3/Giorno4/lezione1/src/app/app.module.ts
+++ UNIT3/Giorno4/lezione1/src/app/app.module.ts
@@ -7,35 +7,52 @@ import { PrimoComponent } from './components/primo/primo.component';
 import { SecondoComponent } from './components/secondo/secondo.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { Error404Component } from './components/error404/error404.component';
+import { SecondoAComponent } from './components/secondo-a/secondo-a.component';
+import { SecondoBComponent } from './components/secondo-b/secondo-b.component';
 
 const routes: Route[] = [
     {
         path: '',
-        component: HomeComponent
+        component: HomeComponent,
     },
     {
         path: 'primo',
-        component: PrimoComponent
+        component: PrimoComponent,
     },
     {
         path: 'secondo',
-        component: SecondoComponent
-    }
-]
+        component: SecondoComponent,
+        children: [
+            {
+                path: 'secondoA',
+                component: SecondoAComponent
+            },
+            {
+                path: 'secondoB',
+                component: SecondoBComponent
+            }
+        ]
+    },
+    {
+        path: '**',
+        component: Error404Component,
+    },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PrimoComponent,
-    SecondoComponent,
-    HomeComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PrimoComponent,
+        SecondoComponent,
+        HomeComponent,
+        NavbarComponent,
+        Error404Component,
+        SecondoAComponent,
+        SecondoBComponent,
+    ],
+    imports: [BrowserModule, RouterModule.forRoot(routes)],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
