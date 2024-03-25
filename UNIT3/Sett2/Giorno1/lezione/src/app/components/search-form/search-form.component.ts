54125a0262904d2cdf61dc5555952bf52f4297c0
Naviganet <dadegi@gmail.com>
2024-03-25 10:13:35 +0100

    Codice lezione UNIT3 settimana 2 giorno 1

--- UNIT3/Sett2/Giorno1/lezione/src/app/components/search-form/search-form.component.ts
+++ UNIT3/Sett2/Giorno1/lezione/src/app/components/search-form/search-form.component.ts
@@ -7,16 +7,13 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class SearchFormComponent {
     @Output() handleData = new EventEmitter<string>();
-    @Output() handleError = new EventEmitter<boolean>()
 
     getData(event: KeyboardEvent) {
         if (event.key === 'Enter') {
             let target = event.target as HTMLInputElement;
             if (target.value != '') {
-                this.handleError.emit(false);
                 this.handleData.emit(target.value);
             } else {
-                this.handleError.emit(true);
                 this.handleData.emit('Inserire un valore nel campo di ricerca');
             }
 
